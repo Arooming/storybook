7068ddfe3fd7e5e1dffaaf11b38a3c1d618e9762
seoAreum <1971236@hansung.ac.kr>
2024-08-05 04:22:38 +0900

    style: tailwind fontSize 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,45 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    // NotoSans, Roboto, Inter 등의 폰트를 사용한다면 `@fontsource`에서 설치하는게 가장 빠름 + 번들러 크기도 작음
+    // `@fontsource`: 다양한 웹 폰트들을 npm 패키지로 해둔 것
+    // 가장 자주 쓰이는 것들 위주로 설정할 것 ! 
+    // 어차피 fontWeight나 lineHeight 같은 것들은 여기서 정의한 걸 override한 후, inline style로 정의한 내용들로 설정됨
+    fontSize: {
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
     colors: {
       primary: "#1d2745",
       secondary: "#1de5d4",
