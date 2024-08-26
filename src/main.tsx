683ceed48acf0a455ecfa7a81f4dafbcdd8cfae9
seoAreum <1971236@hansung.ac.kr>
2024-08-26 15:51:00 +0900

    style: stylelint 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import "@fontsource/noto-sans/400.css"; // Specify weight
-import "@fontsource/noto-sans/4700.css"; // Specify weight
-import './index.css'
+import "@fontsource/noto-sans/700.css"; // Specify weight
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
